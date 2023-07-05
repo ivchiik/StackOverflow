776e8d0584c3932f6ea06ebbb5ce64a749762d50
Iva <ivasaata@gmail.com>
2023-07-05 16:52:34 +0400

    Search

---
+++ app/search.js
@@ -0,0 +1,65 @@
+import { useNavigation } from "expo-router";
+import React, { useState, useLayoutEffect } from "react";
+import {
+  View,
+  Text,
+  ActivityIndicator,
+  SafeAreaView,
+  FlatList,
+} from "react-native";
+import { useQuery } from "urql";
+import { searchQuery } from "../src/graphql/queries";
+import QuestionListItem from "../src/components/QuestionListItem";
+
+const search = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const [result, fetch] = useQuery({
+    query: searchQuery,
+    pause: true,
+    variables: { term: searchTerm },
+  });
+
+  const navigation = useNavigation();
+
+  const search = () => {
+    fetch();
+    console.warn("Search: ", searchTerm);
+  };
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerSearchBarOptions: {
+        onChangeText: (event) => setSearchTerm(event.nativeEvent.text),
+        onBlur: search,
+      },
+    });
+  }, [navigation, searchTerm, setSearchTerm]);
+
+  if (result.fetching) {
+    return (
+      <SafeAreaView>
+        <ActivityIndicator style={{ paddingTop: "75%" }} />
+      </SafeAreaView>
+    );
+  }
+
+  if (result.error) {
+    return <Text>Error: {result.error.message}</Text>;
+  }
+
+  if (!result.data) {
+    return <Text>Search for a question</Text>;
+  }
+
+  return (
+    <View style={{ backgroundColor: "#fff", flex: 1 }}>
+      <FlatList
+        data={result.data.search.items}
+        renderItem={({ item }) => <QuestionListItem question={item} />}
+      />
+    </View>
+  );
+};
+
+export default search;
